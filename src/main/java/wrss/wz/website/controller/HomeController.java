bc803dea2b3c893c73d32459996c921c388dc58d
Radoslaw <radoslaw.kalina9@gmail.com>
2020-11-09 23:33:22 +0100

    Initial Commit

---
+++ src/main/java/wrss/wz/website/controller/HomeController.java
@@ -0,0 +1,20 @@
+package wrss.wz.website.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public ResponseEntity<?> home() {
+        return new ResponseEntity<>("WRSS WZ Website", HttpStatus.OK);
+    }
+
+    @PostMapping("/api/student")
+    public ResponseEntity<?> addStudent() {
+        return new ResponseEntity<>("New student added", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/wrss/wz/website/entity/Student.java
@@ -0,0 +1,24 @@
+package wrss.wz.website.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
