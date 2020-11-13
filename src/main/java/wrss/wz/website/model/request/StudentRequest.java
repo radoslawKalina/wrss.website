f79cdee56ebbaabf9e0a7b475dcb1756b28fb98f
Radoslaw <radoslaw.kalina9@gmail.com>
2020-11-13 14:09:24 +0100

    Add test endpoints for database operations

---
+++ src/main/java/wrss/wz/website/model/request/StudentRequest.java
@@ -0,0 +1,12 @@
+package wrss.wz.website.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentRequest {
+    private String name;
+}
