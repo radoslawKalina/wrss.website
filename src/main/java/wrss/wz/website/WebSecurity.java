0ec0f06968b52b36635ce68240a297cb8c632d9e
Radoslaw <radoslaw.kalina9@gmail.com>
2020-11-16 00:36:40 +0100

    Create basics for Spring Security

---
+++ src/main/java/wrss/wz/website/WebSecurity.java
@@ -0,0 +1,19 @@
+package wrss.wz.website;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/user")
+                .permitAll();
+    }
+}
