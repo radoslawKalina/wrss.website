122dc63bcda57507b7e9009c0fcff5b320f22e7a
Radoslaw <radoslaw.kalina9@gmail.com>
2020-11-17 00:12:14 +0100

    Add configuration annotation

--- src/main/java/wrss/wz/website/WebsiteApplication.java
+++ src/main/java/wrss/wz/website/WebsiteApplication.java
@@ -3,9 +3,13 @@ package wrss.wz.website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
