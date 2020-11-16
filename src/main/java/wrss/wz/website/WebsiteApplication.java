df0f4458f564b5e835159903bb89842b19868558
Radoslaw <radoslaw.kalina9@gmail.com>
2020-11-17 00:15:06 +0100

    Add entity scanning

--- src/main/java/wrss/wz/website/WebsiteApplication.java
+++ src/main/java/wrss/wz/website/WebsiteApplication.java
@@ -2,6 +2,7 @@ package wrss.wz.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EntityScan("wrss.wz.website.entity")
 public class WebsiteApplication {
 
 	public static void main(String[] args) {
